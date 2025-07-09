5958f48118591d040035236dee43fd0bf1951fff
wyett-gptzero <wyett@gptzero.me>
2025-07-09 19:37:12 -0400

    updates

---
+++ classify.sh
@@ -0,0 +1,6 @@
+python classification.py \
+    --response_paths claude.json openai.json \
+    --classifier bert \
+    --output_dir /path/to/output_dir \
+    --num_test_samples 100 \
+    --num_train_samples 1000
